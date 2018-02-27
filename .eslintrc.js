9b8a13617f90515e0ef5548008390cc1ae6cb4cb
황진수-PC <cjswp122@gmail.com>
2018-02-27 16:49:44 +0900

    first init with flow , eslint setting

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  "extends": [
+      "react-app",
+      "airbnb"
+  ],
+  "settings": {
+    "import/resolver": {
+      node: { paths: [path.resolve('./src')] }
+    },
+  },
+  rules: {
+    "class-methods-use-this": 0,
+    "react/jsx-filename-extension": 0,
+    "jsx-a11y/href-no-hash": 0
+  }
+};
