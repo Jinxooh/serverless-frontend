67842fd811bc5cc6d1812b922a1a459295cc2803
황진수-PC <cjswp122@gmail.com>
2018-02-28 14:44:40 +0900

    eslint setting done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,3 @@
-const path = require('path')
-
 module.exports = {
-  "extends": [
-      "react-app",
-      "airbnb"
-  ],
-  "settings": {
-    "import/resolver": {
-      node: { paths: [path.resolve('./src')] }
-    },
-  },
-  rules: {
-    "class-methods-use-this": 0,
-    "react/jsx-filename-extension": 0,
-    "jsx-a11y/href-no-hash": 0
-  }
+    "extends": "airbnb"
 };
