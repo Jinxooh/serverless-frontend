7c248998cede729df6871b718c0e26d701a95b93
황진수-PC <cjswp122@gmail.com>
2018-03-29 18:03:55 +0900

    spinner auth form 추가구현

--- src/components/home/AuthForm/AuthForm.js
+++ src/components/home/AuthForm/AuthForm.js
@@ -1,20 +1,53 @@
 // @flow
 import React from 'react';
 import SocialLoginButton from 'components/home/SocialLoginButton';
+import CheckIcon from 'react-icons/lib/md/check';
+import Spinner from 'components/common/Spinner';
 
 import './AuthForm.scss';
 
 type Props = {
   onChange(e: Event): void,
+  onEnterKeyPress(e: KeyboardEvent): void,
+  onSendVerification(): Promise<*>,
   email: string,
+  sentEmail: boolean,
+  sending: boolean,
 }
-const AuthForm = ({ onChange, email }: Props) => {
+const AuthForm = ({
+  onChange,
+  email,
+  sentEmail,
+  sending,
+  onEnterKeyPress,
+  onSendVerification,
+}: Props) => {
   return (
     <div className="auth-form">
-      <div className="input-with-button">
-        <input placeholder="Input your E-mail" value={email} onChange={onChange} />
-        <div className="button">Start</div>
-      </div>
+      {
+        sentEmail ? (
+          <div className="sent-email">
+            <CheckIcon />
+            <div className="text">
+              회원가입 인증 링크가 이메일로 전송되었습니다.<br />
+              이메일의 링크를 통하여 회원가입을 계속하세요.
+            </div>
+          </div>
+        ) : (
+          <div className="input-with-button">
+            <input
+              placeholder="Input your E-mail"
+              value={email}
+              disabled={sending}
+              onChange={onChange}
+              onKeyPress={onEnterKeyPress}
+            />
+            <div className="button" onClick={onSendVerification} >
+              {sending ? <Spinner size="3rem" /> : 'Start'}
+            </div>
+          </div>
+        )
+      }
       <div className="separator">
         <div className="or">OR</div>
       </div>
