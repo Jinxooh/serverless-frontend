c38d907705e1734f5fe1979be80d1436c60827b0
황진수-PC <cjswp122@gmail.com>
2018-04-10 14:44:26 +0900

    hoc 구현

--- src/containers/landing/LandingTemplateContainer.js
+++ src/containers/landing/LandingTemplateContainer.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import LandingTemplate from 'components/templates/LandingTemplate';
+import withUser from 'lib/hoc/withUser';
 
-const LandingTemplateContainer = ({ props }) => {
+const LandingTemplateContainer = (props) => {
+  if (props.user) return null;
+
   return (
     <LandingTemplate {...props} />
   );
 };
 
-export default LandingTemplateContainer;
+export default withUser(LandingTemplateContainer);
