a6abd5ee835345fd8b2c1fa90d993a79bd33e01e
황진수-PC <cjswp122@gmail.com>
2018-06-14 13:38:33 +0900

    code editor 개선

--- src/components/write/CodeEditor/CodeEditor.js
+++ src/components/write/CodeEditor/CodeEditor.js
@@ -10,6 +10,7 @@ let CodeMirror = null;
 if (process.env.APP_ENV !== 'server') {
   CodeMirror = require('codemirror');
   require('codemirror/addon/scroll/simplescrollbars');
+  require('codemirror/addon/display/placeholder');
   require('codemirror/mode/markdown/markdown');
   require('codemirror/mode/javascript/javascript');
   require('codemirror/mode/jsx/jsx');
@@ -42,10 +43,11 @@ class CodeEditor extends Component<Props, State> {
     if (!CodeMirror) return;
     this.codeMirror = CodeMirror(this.editor, {
       mode: 'markdown',
-      theme: 'default',
-      lineNumbers: true, // 좌측에 라인넘버 띄우기
+      theme: 'material',
+      lineNumbers: false, // 좌측에 라인넘버 띄우기
       lineWrapping: true, // 내용이 너무 길면 다음 줄에 작성
       scrollbarStyle: 'overlay',
+      placeholder: 'hello',
     });
     this.codeMirror.on('change', this.onChange);
   }
@@ -85,7 +87,7 @@ class CodeEditor extends Component<Props, State> {
 
   render() {
     return (
-      <div className="CodeEditor">
+      <div className="CodeEditor material">
         <div className="editor" ref={(ref) => { this.editor = ref; }} />
         {/* <textarea ref={(ref) => { this.textarea = ref; }} /> */ }
       </div>
