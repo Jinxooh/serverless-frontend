c2ada6c739a3a019c91784d99c2b5aa85341fd85
황진수-PC <cjswp122@gmail.com>
2018-06-26 16:43:53 +0900

    add SubmitBox

---
+++ src/components/write/SubmitBox/SubmitBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './SubmitBox.scss';
+
+class SubmitBox extends Component {
+  render() {
+    return (
+      <div className="SubmitBox">
+        im Submit box
+      </div>
+    );
+  }
+}
+
+export default SubmitBox;
--- src/components/write/WriteHeader/WriteHeader.js
+++ src/components/write/WriteHeader/WriteHeader.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BackIcon from 'react-icons/lib/md/arrow-back';
+import MoreIcon from 'react-icons/lib/md/more-vert';
 import './WriteHeader.scss';
 
 type Props = {
   onChangeTitle(e: any): void,
-  value: string
+  value: string,
+  title: string,
 }
 
 const WriteHeader = ({
@@ -22,7 +24,17 @@ const WriteHeader = ({
           value={title}
         />
       </div>
-      <div className="submit-button">Submit</div>
+      <div className="actions">
+        <div className="button temp-save">
+          Temp
+        </div>
+        <div className="button submit">
+          Submit
+        </div>
+        <div className="more util flex-center">
+          <MoreIcon />
+        </div>
+      </div>
     </div>
   );
 };
