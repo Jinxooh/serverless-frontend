4890dbd76b715a9bc27b7bc1e5e1b5c6b96e3e26
황진수-PC <cjswp122@gmail.com>
2018-07-04 17:50:33 +0900

    CategoryEditItem List Implements

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -5,7 +5,7 @@ import CodeEditorContainer from 'containers/write/CodeEditorContainer';
 import WriteHeaderContainer from 'containers/write/WriteHeaderContainer';
 import MarkdownPreviewContainer from 'containers/write/MarkdownPreviewContainer';
 import SubmitBoxContainer from 'containers/write/SubmitBoxContainer';
-import CategoryEditModal from 'components/write/CategoryEditModal';
+import CategoryEditModalContainer from 'containers/write/CategoryEditModalContainer';
 
 const Write = () => {
   return (
@@ -17,7 +17,7 @@ const Write = () => {
         left={<CodeEditorContainer />}
         right={<MarkdownPreviewContainer />}
       />
-      <CategoryEditModal />
+      <CategoryEditModalContainer />
     </WriteTemplate>
   );
 };
